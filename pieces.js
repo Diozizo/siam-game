ae387874048e36b13b6f11198e6f01568c83fa66
Diozizo <diaeddinemerad@gmail.com>
2024-12-11 20:35:10 +0100

    siam game

---
+++ pieces.js
@@ -0,0 +1,13 @@
+const rhino1 = '<div class="piece" id="rhino" piece-id="rhino"><img src="rhino_1.png"></img></div>';
+const rhino2 = '<div class="piece" id="rhino" piece-id="rhino"><img src="rhino_2.png"></img></div>';
+const rhino3 = '<div class="piece" id="rhino" piece-id="rhino"><img src="rhino_3.png"></img></div>';
+const rhino4 = '<div class="piece" id="rhino" piece-id="rhino"><img src="rhino_4.png"></img></div>';
+const rhino5 = '<div class="piece" id="rhino" piece-id="rhino"><img src="rhino_5.png"></img></div>';
+const elephant1 = '<div class="piece" id="elephant" piece-id="elephant"><img src="elephant_1.png"></img></div>';
+const elephant2 = '<div class="piece" id="elephant" piece-id="elephant"><img src="elephant_2.png"></img></div>';
+const elephant3 = '<div class="piece" id="elephant" piece-id="elephant"><img src="elephant_3.png"></img></div>';
+const elephant4 = '<div class="piece" id="elephant" piece-id="elephant"><img src="elephant_4.png"></img></div>';
+const elephant5 = '<div class="piece" id="elephant" piece-id="elephant"><img src="elephant_5.png"></img></div>';
+const rock1 = '<div class="piece" id="rock" piece-id="rock"><img src="rock_1.png"></img></div>';
+const rock2 = '<div class="piece" id="rock" piece-id="rock"><img src="rock_2.png"></img></div>';
+const rock3 = '<div class="piece" id="rock" piece-id="rock"><img src="rock_3.png"></img></div>';
